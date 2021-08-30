8b8d38388d0ad9128e22e75cc9c4ec7e1b5573ce
Kishor Kumar <kishorr21cse@gmail.com>
2021-08-30 07:02:18 +0530

    first one

---
+++ hello.java
@@ -0,0 +1,10 @@
+class Kishor{
+
+    public static void  main(String args[]) {
+        System.out.println("Hi,Kishore");
+        for(int i=0;i<=90;i++){
+
+            System.out.println(i);
+        }
+    }
+}
