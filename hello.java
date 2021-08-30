46ea5841f782c02dc910650d34936fb5511c9052
Kishor Kumar <kishorr21cse@gmail.com>
2021-08-30 07:03:11 +0530

    second one

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 class Kishor{
 
     public static void  main(String args[]) {
-        System.out.println("Hi,Kishore");
+        System.out.println("Hi,Kishor");
         for(int i=0;i<=90;i++){
 
             System.out.println(i);
